8e43784e825ddaea4b345fdbaa089f033ea6e9fb
Golovina Elnara <70908099+GolovinaElia@users.noreply.github.com>
2023-03-26 20:41:26 +0300

    add changes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,31 @@
-var swiper = new Swiper('.mySwiper', {
-  loop: true,
+// var swiper = new Swiper('.mySwiper', {
+//   loop: true,
+//   autoplay: {
+//     delay: 2000,
+//   },
+//   keyboard: {
+//     enabled: true,
+//   },
+//   breakpoints: {
+    
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 18
+//     },
+
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 18
+//     },
+//   },
+//    pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//   },
+// });
+document.addEventListener('DOMContentLoaded', function () {
+   var productsSlider = new Swiper('.products-slider', {
+   loop: true,
   autoplay: {
     delay: 2000,
   },
@@ -22,4 +48,33 @@ var swiper = new Swiper('.mySwiper', {
     el: '.swiper-pagination',
     type: 'bullets',
   },
+  });
+  
+  var reviewsSlider = new Swiper('.reviews-slider', {
+     loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 18
+    },
+  },
+   pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  });
+  productsSlider[0].controller.control = reviewsSlider;
+  reviewsSlider[0].controller.control = productsSlider;
 });
