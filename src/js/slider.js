2ae58ab331f4b5f4b56875a289bc4fac42712372
Golovina Elnara <70908099+GolovinaElia@users.noreply.github.com>
2023-05-27 15:13:18 +0300

    add changes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,28 +1,3 @@
-// var swiper = new Swiper('.mySwiper', {
-//   loop: true,
-//   autoplay: {
-//     delay: 2000,
-//   },
-//   keyboard: {
-//     enabled: true,
-//   },
-//   breakpoints: {
-    
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 18
-//     },
-
-//     1200: {
-//       slidesPerView: 4,
-//       spaceBetween: 18
-//     },
-//   },
-//    pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//   },
-// });
 document.addEventListener('DOMContentLoaded', function () {
    var productsSlider = new Swiper('.products-slider', {
    loop: true,
@@ -75,6 +50,31 @@ document.addEventListener('DOMContentLoaded', function () {
     type: 'bullets',
   },
   });
+  var productsSlider = new Swiper('.buy-slider', {
+   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    },
+     1200: {
+      slidesPerView: 3,
+      spaceBetween: 16
+    },
+  },
+   pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  });
   productsSlider[0].controller.control = reviewsSlider;
   reviewsSlider[0].controller.control = productsSlider;
+  buySlider[0].controller.control = buySlider;
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import './js/slider';
 import './js/mobile-menu';
 import './js/buy-now';
+import './js/explore';
+import './js/leave';
+import './js/how-made';
